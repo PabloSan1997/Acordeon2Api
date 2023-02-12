744ef68615663bd340d3c1cec3205c3de3b7ee72
PabloSan1997 <jos_pab_997@hotmali.com>
2023-02-11 19:33:17 -0600

    Se arregro problema con CORS

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
+const cors = require('cors');
 const { boomHandle } = require('./middleware/handle.js');
 const { crearApi } = require('./Routers/index.js');
 const PUERTO = process.env.PORT || 3005;
 const app = express();
 
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
 crearApi(app);
 app.use(boomHandle);
 app.get('/', (req, res)=>{
